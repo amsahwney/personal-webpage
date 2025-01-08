5a057e77acf444bf88504a0e0b0b1a905c67c586
Amrita <amritak.saw@gmail.com>
2025-01-08 12:26:27 -0500

    setting up routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,49 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import Homepage from './components/Homepage.jsx'
+import Landing from './components/Landing.jsx'
+import About from './components/About.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+const routes = [
+  { 
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true, 
+        element: <Landing /> 
+      },
+      {
+        path: "home",
+        element: <Homepage />
+      },
+      {
+        path: "about",
+        element: <About />
+      }
+    ]
+  }
+]
+
+const router = createBrowserRouter(routes)
+createRoot(document.getElementById('root')).render(<RouterProvider router={router}/>)
+
+
+//ADDITIONAL ROUTES TO ADD
+      // ,{
+      //   path: "gallery",
+      //   element: <Gallery />
+      // },
+      // {
+      //   path: "gallery/:id",
+      //   element: <Artpieces />
+      // },
+      // {
+      //   path: "letters",
+      //   element: <LetterForm/>
+      // }
+
+//Routes for admin? Receiving letters page. 
